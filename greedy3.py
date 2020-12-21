3def7d5b0762addc249443dc201f20f1353638db
kimminju99 <50831854+kimminju99@users.noreply.github.com>
2020-12-22 03:04:10 +0900

    Create greedy3.py
    숫자 카드 게임

---
+++ greedy3.py
@@ -0,0 +1,15 @@
+# 숫자 카드 게임
+
+# N, M 공백으로 구분하여 입력받기
+n , m = map(int, input().split())
+
+res = 0
+# 한 줄씩 입력받아 확인
+for i in range(n):
+    data = list(map(int, input().split()))
+    # 현재 줄에서 '가장 작은 수' 찾기
+    min_value = min(data)
+    # '가장 작은 수'들 중 가장 큰 수 찾기
+    res = max(res, min_value)
+    
+print(res)
